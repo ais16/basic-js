754565b76c3ace59716562cbd85f89094320fa94
ais16 <54989705+ais16@users.noreply.github.com>
2021-03-31 04:31:39 +0600

    feat: update src

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+ const nextD = '--discard-next';
+  const prevD = '--discard-prev';
+  const nextDb = '--double-next';
+  const prevDb = '--double-prev';
+
+  if (!(arr instanceof Array)) throw new Error();
+
+  let resArr = [...arr];
+
+  for (let i = 0; i < resArr.length; i++) {
+    if (resArr[i] === nextD) {
+      resArr.splice(i, 2, undefined);
+    } else if (resArr[i] === prevD && i===0) {
+      resArr.splice(i, 1);
+    } else if (resArr[i] === prevD) {
+        resArr.splice(i - 1, 2, undefined);
+    } else if (resArr[i] === nextDb) {
+      resArr.splice(i, 1, resArr[i + 1]);
+    } else if (resArr[i] === prevDb) {
+      resArr.splice(i, 1, resArr[i - 1]);
+    }
+  }
+  resArr = resArr.filter((e) => e !== undefined);
+  return resArr;
 };
