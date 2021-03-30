e02ad070d2c8f4f4f8c8e30392690c008fdc6091
ais16 <54989705+ais16@users.noreply.github.com>
2021-03-31 04:40:02 +0600

    feat: update

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,10 +2,10 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-const LL = 0.693;
+const ln = 0.693;
 
 module.exports = function dateSample(sampleActivity) {
-  let k = LL / HALF_LIFE_PERIOD;
+  let k = ln / HALF_LIFE_PERIOD;
   let t = 0;
   let N = parseFloat(sampleActivity);
   if(typeof(sampleActivity) === "string" && arguments.length !== 0 && !isNaN(N) && N > 0 && N < MODERN_ACTIVITY){
@@ -15,3 +15,4 @@ module.exports = function dateSample(sampleActivity) {
   }else{
     return 0;
   }
+};
